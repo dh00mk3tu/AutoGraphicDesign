02764e989332c61996f6798e019fdbb428d2d35d
dh00mk3tu <cyberhybird@hotmail.com>
2021-11-20 17:22:58 +0530

    completely cutamated

--- main.py
+++ main.py
@@ -1,38 +1,67 @@
 
 # Importing the libraries
+
 import requests
 import numpy as np
 import os
+import glob
 from PIL import Image, ImageDraw, ImageFilter, ImageOps
 
 finalPath = os.getcwd() + '/final/'
 if not os.path.exists(finalPath):
     os.makedirs(finalPath)
 
+profilePath = []
+d = "profiles"
+nobg = "nobg"
+
+#Importing all the image files
+def getProfilePath(pathValue): 
+    profilePath.clear()
+    for path in os.listdir(pathValue):
+        full_path = os.path.join(pathValue, path)
+        if os.path.isfile(full_path):
+            profilePath.append(full_path)
+            
+    print(profilePath)
+
+
 
 #API Call for removing the background
-def removeBackground():
+def removeBackground(path, index):
+    print("remove background called - inside")
+    print(path)
+    print(index)
+    local = "nobg/"+str(index)+".png"
     response = requests.post(
         'https://api.remove.bg/v1.0/removebg',
-        files={'image_file': open('3.jpg', 'rb')},
+        files={'image_file': open(path, 'rb')},
         data={'size': 'auto'},
-        headers={'X-Api-Key': 'ue5DscsAx1rtoDxZT2a7a9SU'},
+        #X3GeXtGGgT2ZiGovgW56egQd - Harshdeep's API Key
+        #ue5DscsAx1rtoDxZT2a7a9SU - Anirudh's API Key
+        headers={'X-Api-Key': 'X3GeXtGGgT2ZiGovgW56egQd'},
     )
     if response.status_code == requests.codes.ok:
-        with open('no-bg.png', 'wb') as out:
+        with open(local, 'wb') as out:
             out.write(response.content)
     else:
         print("Error:", response.status_code, response.text)
+    
+def addBackground(index):
+    getProfilePath(nobg)
+    print(profilePath)
 
-def addBackground():
+    local = "withbg/"+str(index)+".png"
+
     #Open the images
     bg = Image.open('Background.png')
-    overlay = Image.open('no-bg.png')
-
-    #Convert them to RGBA Color schema
-    overlay = overlay.convert("RGBA")
     bg = bg.convert("RGBA")
+
     
+    overlay = Image.open(profilePath[index])
+    overlay = overlay.convert("RGBA")
+
+
     x = overlay.width
     y = overlay.height
     if(y<=x):
@@ -46,26 +75,38 @@ def addBackground():
     width = (bg.width - overlay.width) // 2
     height = (bg.height - overlay.height)
     
-
-
     #Pasting the image
     bg.paste(overlay, (width, height), overlay)
 
     #Saving the image and opening it as output Image
-    bg.save("new.png", format="png")
+    bg.save(local, format="png")
 
 
-def addMask():
-    outputImg = Image.open('new.png')
+def addMask(index):
+    local = "finalPath/"+str(index)+".png"
+    print("add mask called")
+    outputImg = Image.open(profilePath[index])
     mask = Image.open('mask.png').convert('L')
     output = ImageOps.fit(outputImg, mask.size, centering=(0.5, 0.5))
     output.putalpha(mask)   
+    output.save(local)
 
-    output.save(f"{finalPath}/output.png")
+#Main Function
+def main():
+    getProfilePath(d)
+    for i in range(len(profilePath)):
+        print("remove background called")
+        removeBackground(profilePath[i], i)
+    getProfilePath(nobg)
+    for i in range(len(profilePath)):
+        print("add background called")
+        addBackground(i)
+    for i in range(len(profilePath)):
+        addMask(i)
+main()
+# removeBackground()
+# addBackground()
+# addMask()
 
-
-removeBackground()
-addBackground()
-addMask()
 
 
